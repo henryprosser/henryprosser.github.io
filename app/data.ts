839b805ca8a4d02568ada7b1dd9e6f5fd1d1737a
henryprosser <henryprosser@yahoo.co.uk>
2025-09-09 10:07:04 +0100

    Add Projects and Work Experience sections

--- app/data.ts
+++ app/data.ts
@@ -38,39 +38,54 @@ type SocialLink = {
 
 export const PROJECTS: Project[] = [
   {
-    name: 'Motion Primitives Pro',
+    name: 'Chef Claude',
     description:
-      'Advanced components and templates to craft beautiful websites.',
-    link: 'https://pro.motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+      'AI recipe generator that turns ingredients into bespoke step-by-step recipes.',
+    link: 'https://chef-claude-eight.vercel.app/',
+    video: '/chef-claude.mp4',
     id: 'project1',
   },
   {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    name: 'Tenzies',
+    description:
+      'A fast dice game where players roll dice until all ten show the same face.',
+    link: 'https://tenzies-game-nine-mu.vercel.app/',
+    video: '/tenzies.mp4',
     id: 'project2',
+  },
+  {
+    name: 'Assembly: Endgame',
+    description:
+      'A Hangman-style game where players attempt to guess a secret word.',
+    link: 'https://assembly-endgame-hangman-nine.vercel.app/',
+    video: '/ae.mp4',
+    id: 'project3',
+  },
+  {
+    name: 'Travel Journal',
+    description:
+      'A modern web app for documenting and sharing travel experiences.',
+    link: 'https://travel-journal-nine-xi.vercel.app/',
+    video: '/tj.mp4',
+    id: 'project4',
   },
 ]
 
 export const WORK_EXPERIENCE: WorkExperience[] = [
   {
-    company: 'Reglazed Studio',
-    title: 'CEO',
-    start: '2024',
-    end: 'Present',
-    link: 'https://ibelick.com',
+    company: 'Brokit',
+    title: 'Software Developer (Intern)',
+    start: 'Jul 2024',
+    end: 'Dec 2024',
+    link: 'https://www.brokit.co.uk',
     id: 'work1',
   },
   {
-    company: 'Freelance',
-    title: 'Design Engineer',
-    start: '2022',
-    end: '2024',
-    link: 'https://ibelick.com',
+    company: 'Knibbs',
+    title: 'Web Design Assistant',
+    start: 'Apr 2017',
+    end: 'May 2017',
+    link: 'https://knibbs.co.uk',
     id: 'work2',
   },
 ]
@@ -94,52 +109,52 @@ export const EDUCATION: Education[] = [
   },
 ]
 
-export const BLOG_POSTS: BlogPost[] = [
-  {
-    title: 'Exploring the Intersection of Design, AI, and Design Engineering',
-    description: 'How AI is changing the way we design',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-1',
-  },
-  {
-    title: 'Why I left my job to start my own company',
-    description:
-      'A deep dive into my decision to leave my job and start my own company',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-2',
-  },
-  {
-    title: 'What I learned from my first year of freelancing',
-    description:
-      'A look back at my first year of freelancing and what I learned',
-    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
-    uid: 'blog-3',
-  },
-  {
-    title: 'How to Export Metadata from MDX for Next.js SEO',
-    description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
-    link: '/blog/example-mdx-metadata',
-    uid: 'blog-4',
-  },
-]
+// export const BLOG_POSTS: BlogPost[] = [
+//   {
+//     title: 'Exploring the Intersection of Design, AI, and Design Engineering',
+//     description: 'How AI is changing the way we design',
+//     link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+//     uid: 'blog-1',
+//   },
+//   {
+//     title: 'Why I left my job to start my own company',
+//     description:
+//       'A deep dive into my decision to leave my job and start my own company',
+//     link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+//     uid: 'blog-2',
+//   },
+//   {
+//     title: 'What I learned from my first year of freelancing',
+//     description:
+//       'A look back at my first year of freelancing and what I learned',
+//     link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+//     uid: 'blog-3',
+//   },
+//   {
+//     title: 'How to Export Metadata from MDX for Next.js SEO',
+//     description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
+//     link: '/blog/example-mdx-metadata',
+//     uid: 'blog-4',
+//   },
+// ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
     label: 'Github',
-    link: 'https://github.com/ibelick',
+    link: 'https://github.com/henryprosser',
   },
   {
     label: 'Twitter',
-    link: 'https://twitter.com/ibelick',
+    link: 'https://twitter.com/ProsserHenry',
   },
   {
     label: 'LinkedIn',
-    link: 'https://www.linkedin.com/in/ibelick',
+    link: 'https://www.linkedin.com/in/henry-prosser-2b9aa512a/',
   },
   {
     label: 'Instagram',
-    link: 'https://www.instagram.com/ibelick',
+    link: 'https://www.instagram.com/henryprosser_/',
   },
 ]
 
-export const EMAIL = 'your@email.com'
+export const EMAIL = 'henryprosser@icloud.com'
