eea25961a2bae201509151a115a9d91842c338f4
Henry Prosser <44871861+henryprosser@users.noreply.github.com>
2026-01-30 14:20:10 +0000

    style: update footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,7 +70,7 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
+        <a href="https://motion-primitives.com/" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
             <span>© {new Date().getFullYear()} Henry Prosser.</span>
             <span>Built with Motion-Primitives.</span>
