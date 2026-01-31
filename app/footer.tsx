55d2e8afb8286a3f1be4689b2ed8f5d1db2607d7
Henry Prosser <44871861+henryprosser@users.noreply.github.com>
2026-01-31 18:18:23 +0000

    style: tweak ui

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,12 +70,9 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://motion-primitives.com/" target="_blank">
-          <TextLoop className="text-xs text-zinc-500">
+          <div className="text-xs text-zinc-500">
             <span>© {new Date().getFullYear()} Henry Prosser.</span>
-            <span>Built with Motion-Primitives.</span>
-          </TextLoop>
-        </a>
+          </div>
         <div className="text-xs text-zinc-400">
           <ThemeSwitch />
         </div>
